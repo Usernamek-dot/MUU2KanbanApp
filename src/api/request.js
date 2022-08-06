b0790ae551fcb5e1183ff38e5794ffaaa5055198
Karol <kypacheco07@misena.edu.co>
2022-08-05 23:30:21 -0500

    connect to api #2

--- src/api/request.js
+++ src/api/request.js
@@ -1,16 +1,19 @@
 const API_URL =
   "https://api.sheety.co/27b5b55a3388c846e87b53ccdb8b1ed0/toDoList/toDo";
 
-//get data from API
-axios
-  .get(`${API_URL}/tasks`)
-  .then((res) => getTasks(res.data))
-  .catch((err) => console.error(err));
-
 // map get request
 const getTasks = (data) => {
   data.map((task) => createTask(task));
+  console.log("data", data);
 };
+
+//get data from API
+axios
+  .get(`${API_URL}`)
+  .then((res) => getTasks(res.data.data), console.log(res))
+  .catch((err) => console.error(err));
+
+// axios.get(`${API_URL}`).then((res) => getTasks(res.data.data));
 
 const createTask = (task) => {
   // Creating card content
